390d9a3ff7cc2738208b8fc8b424f1f7ce871e87
Ailer-h <henriqueailer2@gmail.com>
2024-02-06 11:21:23 -0300

    ok

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <title>Horta Senai</title>
+</head>
+<body>
+
+    <div class="navbar">
+        <img src="images/logo.png">
+    </div>
+
+</body>
+</html>
