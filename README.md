7c1b036916946052a4927c710686bef36dc96ee8
Ailer-h <henriqueailer2@gmail.com>
2024-02-06 21:46:00 -0300

    ok

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 Palheta de cores:
 
-primary = 5F6F52
-secondary = A9B388
-accent = FEFAE0
-accent 2 = B99470
+primary = #5F6F52
+secondary = #A9B388
+accent = #FAEF5D
+neutral 1 = #FEFAE0
+neutral 2 = #B99470
+
+link = https://colorhunt.co/palette/5f6f52a9b388fefae0b99470
--- index.php
+++ index.php
@@ -5,15 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
     <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-    <title>Horta Senai</title>
+    <title>Horta Ouro Verde</title>
 </head>
 <body>
 
     <div class="navbar">
         <img src="images/logo.png">
-        <div class="menu"></div>
-        
+        <div class="menu">
+            <button class="option">Adicionar planta</button>
+            <button class="option">Visualizar database</button>
+        </div>
     </div>
 
+    <div class="banner">
+        <div class="title">
+            <h1>Horta Ouro Verde</h1>
+            <h2>As melhores hortaliças da região!</h2>
+        </div>
+    </div>
 </body>
 </html>
